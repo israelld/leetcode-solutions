86a6c9df93c08c7e97ead3ad5e5c36c3e247c9d7
Israel Lima Dias <islimadias@gmail.com>
2021-12-03 04:05:00 -0300

    Time: 148 ms (64.44%), Space: 42.4 MB (39.64%) - LeetHub

--- two-sum/two-sum.cs
+++ two-sum/two-sum.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        for(int i=0; i<nums.Length; i++){
-            for(int c=i+1; c<nums.Length; c++){
-                if(nums[c] == target -  nums[i]){
-                    int[] indTwoAdd = {i,c};
-                    return indTwoAdd;
+        for(int i = 0; i < nums.Length; i++){
+            for(int j = i+1; j < nums.Length; j++){
+                if(nums[i] + nums[j] == target){
+                    int[] result = {i,j};
+                    return result;
                 }
-            }            
+            }
         }
         return null;
     }
